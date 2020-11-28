db0a76b5b5815a34ea13bf9bf67d961741127bb9
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-11-28 10:16:57 -0800

    Add new 'app' directory.

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/o-header.js';
+
+function App() {
+  return (
+    <div className="o-app">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
