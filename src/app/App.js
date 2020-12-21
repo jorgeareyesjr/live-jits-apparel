70835c78feb6fd41ed3822317fe693b2d8281cf0
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-12-20 18:02:10 -0800

    Add new routes (test out nested routing); Add an effect to determine navigation behavior (based on wether a 'hash' is detected in the URL - if one is present, jump to a specific element within the app, otherwise, jump to the top of a new page).

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,64 @@
+import React, { useEffect } from 'react';
+import { Switch, Route, useLocation, useRouteMatch } from "react-router-dom";
+import Header from './views/components/o-header/o-header.js';
+import Footer from './views/components/o-footer/o-footer.js'
+import Home from './views/pages/home/p-home.js';
+import Categories from './views/pages/category/p-categories.js';
 import './App.css';
-import Header from './components/o-header.js';
+import Product from './views/pages/product/p-product.js';
 
 function App() {
+	const { pathname, hash } = useLocation();
+
+	// Effect to determine navigation behavior.
+	// If a hash in the URL is detected, jump to the specified element, otherwise, tell the router to switch to the top of the selected page.
+	useEffect(() => {
+    let useEffectAborted = false;
+
+    function scrollToTop() {
+      window.scrollTo(0, 0);
+    };
+    function scrollToID() {
+      const id = hash.replace('#', '');
+      const element = document.getElementById(id);
+      
+      if (element) {
+        window.scrollTo({ top: element.offsetTop});
+      }
+    };
+
+    if(!useEffectAborted) {
+      (hash === '') ? scrollToTop() : scrollToID();
+    }
+
+    return (
+      () => {
+        useEffectAborted = true;
+      }
+    );
+  }, [pathname, hash]);
+
   return (
     <div className="o-app">
       <Header/>
+			<hr />
+			<Switch>
+				<Route
+					exact
+					path="/"
+					component={Home}
+				/>
+				<Route
+					path='/categories'
+					component={Categories}
+				/>
+				<Route
+					path='/products/:productId'
+					component={Product}
+				/>
+			</Switch>
+			<hr />
+			<Footer />
     </div>
   );
 }
