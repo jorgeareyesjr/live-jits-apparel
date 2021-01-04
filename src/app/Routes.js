a910fa9da6ae8f550d4201b5f4960394fce8d00b
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2021-01-03 18:33:48 -0800

    Create new 'Routes' file (to contain all relevant routes for the main app).

---
+++ src/app/Routes.js
@@ -0,0 +1,54 @@
+export const Routes = [
+	{
+		id: '001',
+		path: '/',
+		name: 'Home',
+		isPrimary: true,
+	},
+	// @TODO: Add Router
+	{
+		id: '002',
+		path: '/shop/mens-apparel',
+		name: 'Mens Apparel',
+		isPrimary: true,
+	},
+	// @TODO: Add Router
+	{
+		id: '003',
+		path: '/shop/womens-apparel',
+		name: 'Womens Apparel',
+		isPrimary: true,
+	},
+	// @TODO: Add Router
+	{
+		id: '004',
+		path: '/shop/kids-apparel',
+		name: 'Kids Apparel',
+		isPrimary: true,
+	},
+	// @TODO: Add Router
+	{
+		id: '005',
+		path: '/shop/accessories',
+		name: 'Accessories',
+		isPrimary: true,
+	},
+	{
+		id: '006',
+		path: '/shop/categories',
+		name: 'Shop by Category',
+		isPrimary: false,
+	},
+	{
+		id: '007',
+		path: '/shop/collections',
+		name: 'Shop by Collection',
+		isPrimary: false,
+	},
+	{
+		id: '008',
+		path: '/shop/products',
+		name: 'Shop all Products',
+		isPrimary: false,
+	},
+];
